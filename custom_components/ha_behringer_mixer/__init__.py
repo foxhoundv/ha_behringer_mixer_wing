d8767538f379ee6214b26e18f89e621b2a0ca2d0
foxhoundv <travis.m.patton@gmail.com>
2025-12-31 11:57:01 -0700

    Update __init__.py
    Adding Automation Recorder and Player

--- custom_components/ha_behringer_mixer/__init__.py
+++ custom_components/ha_behringer_mixer/__init__.py
@@ -8,6 +8,8 @@ from homeassistant.core import HomeAssistant
 from .api import BehringerMixerApiClient
 from .const import DOMAIN, LOGGER
 from .coordinator import MixerDataUpdateCoordinator
+from .automation_recorder import AutomationRecorder
+from .automation_player import AutomationPlayer
 
 PLATFORMS: list[Platform] = [
     Platform.SWITCH,
@@ -105,3 +107,61 @@ async def async_migrate_entry(hass, config_entry: ConfigEntry):
     LOGGER.debug("Migration to version %s successful", config_entry.version)
 
     return True
+
+class BehringerWingMixer:
+    def __init__(self, ...):
+        # ... existing code ...
+        self.automation_recorder = AutomationRecorder()
+        self.automation_player = AutomationPlayer(self.osc_client)
+        self.armed_channels = set()  # Channels enabled for automation
+        
+    async def async_setup_services(self):
+        """Register automation services"""
+        
+        async def handle_record_start(call):
+            project_name = call.data.get("project_name")
+            initial_state = await self._capture_current_state()
+            self.automation_recorder.start_recording(initial_state)
+            
+        async def handle_record_stop(call):
+            save_path = call.data.get("save_path")
+            automation_data = self.automation_recorder.stop_recording()
+            await self._save_automation(save_path, automation_data)
+            
+        async def handle_play_automation(call):
+            automation_file = call.data.get("automation_file")
+            from_position = call.data.get("from_position", 0.0)
+            await self.automation_player.load_automation(automation_file)
+            await self.automation_player.start_playback(from_position)
+            
+        async def handle_stop_automation(call):
+            await self.automation_player.stop_playback()
+            
+        async def handle_arm_channels(call):
+            channels = self._parse_channel_list(call.data.get("channels", ""))
+            buses = self._parse_channel_list(call.data.get("buses", ""))
+            enable = call.data.get("enable", True)
+            
+            if enable:
+                self.armed_channels.update(channels)
+                self.armed_channels.update([(b, "bus") for b in buses])
+            else:
+                self.armed_channels.difference_update(channels)
+                self.armed_channels.difference_update([(b, "bus") for b in buses])
+        
+        # Register services
+        self.hass.services.async_register(
+            DOMAIN, "record_automation_start", handle_record_start
+        )
+        self.hass.services.async_register(
+            DOMAIN, "record_automation_stop", handle_record_stop
+        )
+        self.hass.services.async_register(
+            DOMAIN, "play_automation", handle_play_automation
+        )
+        self.hass.services.async_register(
+            DOMAIN, "stop_automation", handle_stop_automation
+        )
+        self.hass.services.async_register(
+            DOMAIN, "automation_arm_channels", handle_arm_channels
+        )
