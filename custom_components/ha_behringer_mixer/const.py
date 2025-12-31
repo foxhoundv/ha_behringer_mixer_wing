c8822346837a23e5686bf75f39da77f1e138eff0
foxhoundv <travis.m.patton@gmail.com>
2025-12-31 11:26:48 -0700

    Update const.py
    Focus only on Wing. Reset version. Rename "Name" and "Domain"

--- custom_components/ha_behringer_mixer/const.py
+++ custom_components/ha_behringer_mixer/const.py
@@ -1,9 +1,10 @@
-"""Constants for Behringer Mixer integration."""
+"""Constants for Behringer Wing Mixer integration."""
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Behringer Mixer"
-DOMAIN = "ha_behringer_mixer"
-VERSION = "0.1.10"
+NAME = "Behringer Wing Mixer"
+DOMAIN = "ha_behringer_mixer_wing"
+VERSION = "0.1.1"
 ATTRIBUTION = ""
+MIXER_TYPES = ["WING"]  # Remove X32, M32, XR18, etc.
